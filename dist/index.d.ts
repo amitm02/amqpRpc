f4b35170ffd6b269092f44768dbca7c3dd73dd62
amitm02 <amit.man@gmail.com>
2019-06-16 13:08:19 +0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export * from './amqpRpcClient';
 export * from './amqpRpcServer';
 export * from './rabbitmqMonitor';
+export { Subject } from 'rxjs';
--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./amqpRpcClient"));
 __export(require("./amqpRpcServer"));
 __export(require("./rabbitmqMonitor"));
+var rxjs_1 = require("rxjs");
+exports.Subject = rxjs_1.Subject;
 //# sourceMappingURL=index.js.map
